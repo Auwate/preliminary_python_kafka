9f14d3e68c47dc451fa134d426e2b9db69a2ad82
Auwate <auwate@DESKTOP-O13SS5H>
2024-09-16 13:09:19 -0400

    Fixing shell script part 5

--- build/containers/consumers/consumer.sh
+++ build/containers/consumers/consumer.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
+REPO_NAME="https://github.com/Auwate/preliminary_python_kafka.git -b testing"
+DIR_NAME="preliminary_python_kafka"
+
 # Clone the repo
-git clone https://github.com/Auwate/preliminary_python_kafka.git -b testing
-cd ./preliminary_python_kafka/
+
+if [ -d "$DIR_NAME" ]; then
+    rm -rf "$DIR_NAME"
+fi
+
+git clone "$REPO_NAME"
+cd "$DIR_NAME"
 
 # Get dependencies
 python3 -m pip install poetry
-ls
 python3 -m poetry install --no-root
 
 # Run consumer application
