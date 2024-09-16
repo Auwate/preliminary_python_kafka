6a2748327941867776f6d706bf1fde8e2223f286
Auwate <auwate@DESKTOP-O13SS5H>
2024-09-16 12:42:36 -0400

    Fixing shell script part 2

--- build/containers/consumers/consumer.sh
+++ build/containers/consumers/consumer.sh
@@ -2,11 +2,11 @@
 
 # Clone the repo
 git clone https://github.com/Auwate/preliminary_python_kafka.git -b testing
-ls preliminary_python_kafka/
 cd preliminary_python_kafka/
 
 # Get dependencies
 python3 -m pip install poetry
+ls
 python3 -m poetry install --no-root
 
 # Run consumer application
